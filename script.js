64fe2325d433baa95b3c89e25990f58a24b6d810
Kai_loni_Casper <4800627439@davistech.edu>
2023-04-25 15:22:41 -0600

    Multi-level function + finished

--- script.js
+++ script.js
@@ -18,21 +18,58 @@ levels[0] = {
     },
     theme:'default'
 };
+levels[1] = {
+    map: [
+        [1,0,1,1,1,1],
+        [0,0,0,0,0,0],
+        [0,1,1,1,0,0],
+        [0,0,0,1,1,0],
+        [0,1,0,1,0,0]
+    ],
+    player:{
+        x:2,
+        y:4
+    },
+    goal:{
+        x:4,
+        y:4
+    },
+    theme: 'grassland'
+};
+levels[2] = {
+    map: [
+        [1,0,1,0,0,1,0],
+        [0,0,0,0,0,1,0],
+        [1,0,1,1,0,0,0],
+        [1,0,0,1,0,1,0],
+        [1,1,0,0,1,0,0]
+    ],
+    player: {
+        x:2,
+        y:4
+    },
+    goal:{
+        x:6,
+        y:4
+    },
+    theme: 'dungeon'
+};
 function Game(id, level) {
   
     this.el = document.getElementById(id);
+    this.level_idx = 0;
     this.tileTypes = ['floor','wall'];
     this.tileDim = 32;
   
     this.map = level.map;
-    this.theme = level.theme
+    this.theme = level.theme;
     this.player = {...level.player};
     this.goal = {...level.goal};
 }
 
 Game.prototype.populateMap = function() {
     this.el.className = 'game-container ' + this.theme;
-    let tiles = this.el.querySelector('#tiles');
+    let tiles = document.getElementById('tiles');
     for (var y = 0; y < this.map.length; ++y) {
         for (var x = 0; x < this.map[y].length; ++x) {
             let tileCode = this.map[y][x];
@@ -50,20 +87,20 @@ Game.prototype.createEl = function(x,y,type) {
     el.style.top = y*this.tileDim + 'px';
     return el;
 }
-Game.prototype.sizeUp = function() {
-    let map  = this.el.querySelector('.game-map');
-    map.style.height = this.map.length * this.tileDim + 'px';
-    map.style.width = this.map[0].length * this.tileDim + 'px';
-}
 Game.prototype.placeSprite = function(type) {
-    let x = this[type].x
-    let y = this[type].y
-    let sprite = this.createEl(x, y, type);
+    let x = this[type].x;
+    let y = this[type].y;
+    let sprite = this.createEl(x,y,type);
     sprite.id = type;
     sprite.style.borderRadius = this.tileDim + 'px';
     let layer = this.el.querySelector('#sprites');
     layer.appendChild(sprite);
     return sprite;
+}
+Game.prototype.sizeUp = function() {
+    let map  = this.el.querySelector('.game-map');
+    map.style.height = this.map.length * this.tileDim + 'px';
+    map.style.width = this.map[0].length * this.tileDim + 'px';
 }
 Game.prototype.movePlayer = function(event) {
     event.preventDefault();
@@ -88,47 +125,55 @@ Game.prototype.movePlayer = function(event) {
 }
 Game.prototype.moveLeft = function (sprite) {
     if (this.player.x == 0) {
+        this.collide();
         return;
     }
     let nextTile = this.map[this.player.y][this.player.x - 1];
     if (nextTile == 1) {
+        this.collide();
         return;
     }
     this.player.x -= 1;
     this.updateHoriz(sprite);
 }
-Game.prototype.moveUp = function (sprite) {
+Game.prototype.moveUp = function () {
     if (this.player.y == 0) {
+        this.collide();
         return;
     }
     let nextTile = this.map[this.player.y - 1][this.player.x];
     if (nextTile == 1) {
+        this.collide();
         return;
     }
     this.player.y -= 1;
-    this.updateVert(sprite);
+    this.updateVert();
 }
 Game.prototype.moveRight = function (sprite) {
     if (this.player.x == this.map[this.player.y].length - 1) {
+        this.collide();
         return;
     }
     let nextTile = this.map[this.player.y][this.player.x + 1];
     if (nextTile == 1) {
+        this.collide();
         return;
     }
     this.player.x += 1;
     this.updateHoriz(sprite);
 }
-Game.prototype.moveDown = function (sprite) {
+Game.prototype.moveDown = function () {
     if (this.player.y == this.map.length - 1) {
+        this.collide();
         return;
     }
     let nextTile = this.map[this.player.y + 1][this.player.x];
     if (nextTile == 1) {
+        this.collide();
         return;
     }
     this.player.y += 1;
-    this.updateVert(sprite);
+    this.updateVert();
 }
 Game.prototype.updateHoriz = function (sprite) {
     this.player.el.style.left = this.player.x * this.tileDim + 'px';
@@ -138,7 +183,6 @@ Game.prototype.updateVert = function () {
 }
 Game.prototype.checkGoal = function (instrux_msg, goal_msg) {
     let body = document.querySelector('body');
-    let txt = this.el.querySelector('.text');
     if (this.player.y == this.goal.y &&
         this.player.x == this.goal.x) {
         body.className = 'success';
@@ -152,7 +196,7 @@ Game.prototype.keyboardListener = function() {
         this.checkGoal();
     });
 }
-Game.prototype.buttonListeners = function(instrux_msg,goal_msg) {
+Game.prototype.buttonListeners = function(instrux_msg, goal_msg) {
     let up = document.getElementById('up');
     let left = document.getElementById('left');
     let down = document.getElementById('down');
@@ -176,15 +220,59 @@ Game.prototype.buttonListeners = function(instrux_msg,goal_msg) {
         obj.checkGoal(instrux_msg, goal_msg);
     });
 }
+Game.prototype.changeLevel = function() {
+    this.level_idx++;
+    if (this.level_idx > levels.length - 1) {
+        this.level_idx = 0;
+    }
+    let level = levels[this.level_idx];
+    this.map = level.map;
+    this.theme = level.theme;
+    this.player = {...level.player};
+    this.goal = {...level.goal};
+}
+Game.prototype.addMazeListener = function() {
+    let map = this.el.querySelector('.game-map');
+    let obj = this;
+    map.addEventListener('mousedown', function(e) {
+        if (obj.player.y != obj.goal.y ||
+            obj.player.x != obj.goal.x) {
+            return;
+        }
+        obj.changeLevel();
+        let layers = obj.el.querySelectorAll('.layer');
+        for (layer of layers) {
+            layer.innerHTML = '';
+        }
+        obj.placeLevel();
+        obj.checkGoal();
+    });
+}
+Game.prototype.placeLevel = function() {
+    this.populateMap();
+    this.sizeUp();
+    this.placeSprite('goal');
+    let playerSprite = this.placeSprite('player');
+    this.player.el = playerSprite;
+    
+}
+Game.prototype.addListeners = function() {
+    this.keyboardListener();
+    this.buttonListeners();
+    this.addMazeListener();
+}
+Game.prototype.collide = function() {
+    this.player.el.className += ' collide';
+    let delay = 200;
+    let obj = this;
+    window.setTimeout(function() {
+        obj.player.el.className = 'player';
+    }, delay);
+    return 0;
+}
 function init() {
     let myGame = new Game('game-container-1',levels[0]);
-    
-    myGame.populateMap();
-    myGame.sizeUp();
-    myGame.placeSprite('goal');
-    let playerSprite = myGame.placeSprite('player');
-    myGame.player.el = playerSprite;
-    myGame.keyboardListener();
-    myGame.buttonListeners();
+    myGame.placeLevel();
+    myGame.addListeners();
 }
 init();
